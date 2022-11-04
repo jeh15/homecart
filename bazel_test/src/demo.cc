36b5b6a38d61cefc925386fa41f3a3f9fcdec777
jeh15 <jeh15@fsu.edu>
2022-11-04 18:01:05 -0400

    bazel testing

--- bazel_test/src/demo.cc
+++ bazel_test/src/demo.cc
@@ -22,73 +22,73 @@ std::vector<double> get_target(const std::vector<double>& pose, double linear_st
 }
 
 int main(int argc, char* argv[]){
-    // // Initialize Control Interface and Connect to UR SIM:
-    // ur_rtde::RTDEControlInterface rtde_control("192.168.4.30");
-    // ur_rtde::RTDEReceiveInterface rtde_receive("192.168.4.30");
+    // Initialize Control Interface and Connect to UR SIM:
+    ur_rtde::RTDEControlInterface rtde_control("192.168.4.30");
+    ur_rtde::RTDEReceiveInterface rtde_receive("192.168.4.30");
 
-    // // Check Initialization:
-    // std::cout << "Controller and Receiver Initialized!" << std::endl;
+    // Check Initialization:
+    std::cout << "Controller and Receiver Initialized!" << std::endl;
 
-    // // Check Connection:
-    // std::cout << "Is Connected: " << rtde_control.isConnected() << std::endl;
+    // Check Connection:
+    std::cout << "Is Connected: " << rtde_control.isConnected() << std::endl;
 
-    // // Initialize LCM:
-    // lcm::LCM lcm;
-    // if(!lcm.good()){
-    //     std::cout << "LCM Initialization Failed!" << std::endl;
-    //     return 1;
-    // }
-    // std::cout << "LCM Initialization Success!" << std::endl;
+    // Initialize LCM:
+    lcm::LCM lcm;
+    if(!lcm.good()){
+        std::cout << "LCM Initialization Failed!" << std::endl;
+        return 1;
+    }
+    std::cout << "LCM Initialization Success!" << std::endl;
 
-    // // Gripper Command Template:
-    // lcmt_schunk_wsg_command schunk_command;
-    // schunk_command.target_position_mm = 100;
-    // schunk_command.force = 10;
+    // Gripper Command Template:
+    lcmt_schunk_wsg_command schunk_command;
+    schunk_command.target_position_mm = 100;
+    schunk_command.force = 10;
 
-    // // Set Gripper to known state:
-    // lcm.publish("SCHUNK_WSG_COMMAND", &schunk_command);
+    // Set Gripper to known state:
+    lcm.publish("SCHUNK_WSG_COMMAND", &schunk_command);
 
-    // // Sleep and wait for gripper (2s)
-    // std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    // Sleep and wait for gripper (2s)
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
-    // // Setup UR Control Parameters:
-    // double velocity = 0.1;
-    // double acceleration = 0.1;
-    // double dt = 1.0 / 500; // 2ms
-    // double lookahead_time = 0.1;
-    // double gain = 500;
-    // constexpr auto target_freq = std::chrono::milliseconds(2);
-    // std::vector<double> initial_pose = rtde_receive.getActualTCPPose();
+    // Setup UR Control Parameters:
+    double velocity = 0.1;
+    double acceleration = 0.1;
+    double dt = 1.0 / 500; // 2ms
+    double lookahead_time = 0.1;
+    double gain = 500;
+    constexpr auto target_freq = std::chrono::milliseconds(2);
+    std::vector<double> initial_pose = rtde_receive.getActualTCPPose();
 
-    // // Grab Block:
-    // schunk_command.target_position_mm = 10;
-    // schunk_command.force = 10;
-    // lcm.publish("SCHUNK_WSG_COMMAND", &schunk_command);
+    // Grab Block:
+    schunk_command.target_position_mm = 10;
+    schunk_command.force = 10;
+    lcm.publish("SCHUNK_WSG_COMMAND", &schunk_command);
 
-    // // Sleep and wait for gripper (1s)
-    // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    // Sleep and wait for gripper (1s)
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-    // // Move to Target Position:
-    // std::vector<double> init_pose = get_target(initial_pose);
-    // rtde_control.moveL(init_pose);
-    // std::this_thread::sleep_for(target_freq);
+    // Move to Target Position:
+    std::vector<double> init_pose = get_target(initial_pose);
+    rtde_control.moveL(init_pose);
+    std::this_thread::sleep_for(target_freq);
 
-    // // Hold block for:
-    // std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    // Hold block for:
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
-    // // Drop Block:
-    // schunk_command.target_position_mm = 100;
-    // schunk_command.force = 10;
-    // lcm.publish("SCHUNK_WSG_COMMAND", &schunk_command);
+    // Drop Block:
+    schunk_command.target_position_mm = 100;
+    schunk_command.force = 10;
+    lcm.publish("SCHUNK_WSG_COMMAND", &schunk_command);
 
-    // // Sleep and wait for gripper (1s)
-    // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    // Sleep and wait for gripper (1s)
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-    // // Stop Procedure:
-    // rtde_control.servoStop();
-    // std::cout << "Servo Ended..." << std::endl;
-    // rtde_control.stopScript();
-    // std::cout << "Script Ended..." << std::endl;
+    // Stop Procedure:
+    rtde_control.servoStop();
+    std::cout << "Servo Ended..." << std::endl;
+    rtde_control.stopScript();
+    std::cout << "Script Ended..." << std::endl;
 
     return 0;
 }
--- bazel_test/usr_include.BUILD
+++
@@ -1,7 +0,0 @@
-# usr_include.BUILD
-cc_library(
-  name = "ur_rtde",
-  hdrs = glob(["include/ur_rtde/**"]),
-  includes = ["include/ur_rtde"],
-  visibility = ["//visibility:public"],
-)
